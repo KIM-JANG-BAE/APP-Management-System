e7832a8db55785a563ade2114466b6f3c2fe8691
KIM-JANG-BAE <rlawkdqo159@naver.com>
2022-03-10 15:01:37 +0900

    added APP Management System

---
+++ src/ClassProject_PhoneAPP_Managemant.java
@@ -0,0 +1,16 @@
+
+
+public class ClassProject_PhoneAPP_Managemant {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Kim.J.B's phone APP");
+		System.out.println("1. APP List");
+		System.out.println("2. APP Download");
+		System.out.println("3. APP Delete");
+		System.out.println("4. APP Update");
+		System.out.println("5. Exit");
+		
+	}
+
+}
