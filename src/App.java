8693c9bf0e3beb4254cd45e84a19d6342aa1ad31
KIM-JANG-BAE <rlawkdqo159@naver.com>
2022-04-02 17:07:07 +0900

    add array

--- src/App.java
+++ src/App.java
@@ -1,19 +1,17 @@
 public class App {
-	int code;
-	String name;
-	int capacity;
+	int[] code = new int[100];
+	String[] name = new String[100];
+	int[] capacity = new int[100];
 	
 	public App() {
 		
 	}
-	
-	public App(int code, String name, int capacity) {
-		this.code = code;
-		this.name = name;
-		this.capacity = capacity;
+	public App(int code, String name, int capacity, int num) {
+		this.code[num] = code;
+		this.name[num] = name;
+		this.capacity[num] = capacity;
 	}
-	
-	public void printInfo() {
-		System.out.println("code : " + code + " name : " + name + " capacity : " + capacity);
+	public void printInfo(int num) {
+		System.out.println("code : " + code[num] + " name : " + name[num] + " capacity : " + capacity[num]);
 	}
 }
--- src/AppManager.java
+++ src/AppManager.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class AppManager {
-	App App;
+	App App = new App();
 	Scanner input;
 	
 	AppManager(Scanner input){
@@ -9,45 +9,59 @@ public class AppManager {
 	}
 	
 	public void App_download() {
-		App = new App();
+		System.out.print("App order:");
+		int num1 = input.nextInt();
 		System.out.print("APP code:");
-    	App.code = input.nextInt();
+    	App.code[num1] = input.nextInt();
     	input.nextLine();
     	System.out.print("APP name:");
-    	App.name = input.nextLine();
+    	App.name[num1] = input.nextLine();
     	System.out.print("APP capacity:");
-    	App.capacity = input.nextInt();
+    	App.capacity[num1] = input.nextInt();
 	}
 	
 	public void App_delete()
 	{
+		System.out.print("App order:");
+		int num1 = input.nextInt();
 		System.out.print("APP code:");
 		int appcode = input.nextInt();
-		if(App == null) {
+		if(App.code == null) {
+			System.out.println("Any App has not been download");
+			return;
+		}
+		else if(App.code[num1] != appcode) {
 			System.out.println("the App has not been download");
 			return;
 		}
-		else if(App.code == appcode){
-			App = null; 
+		else if(App.code[num1] == appcode){
+			App.code[num1] = 0; 
 			System.out.println("the App is deleted.");
 		}
 	}
 	
 	public void App_update()
 	{
+		System.out.print("App order:");
+		int num1 = input.nextInt();
 		System.out.print("APP code:");
 		int appcode = input.nextInt();
-		if(App.code == appcode) {
+		if(App.code[num1] == appcode) {
 			System.out.println("the App to be update is " + appcode);
+			System.out.println("Update App code data :");
+			int replace1 = input.nextInt();
+			App.code[num1] = replace1;
 		}
 	}
 	
 	public void App_list()
 	{
+		System.out.print("App order:");
+		int num1 = input.nextInt();
 		System.out.print("APP code:");
 		int appcode = input.nextInt();
-		if(App.code == appcode) {
-			App.printInfo();
+		if(App.code[num1] == appcode) {
+			App.printInfo(num1);
 		}
 	}
 
